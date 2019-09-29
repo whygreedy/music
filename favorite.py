0197e3b229e967e02c83695ddc22c56c5cc189b4
whygreedy <cindyyeah9@gmail.com>
2019-09-29 23:51:22 +0800

    add read file

--- favorite.py
+++ favorite.py
@@ -1,5 +1,17 @@
+import os
 music = []
 # 讀取檔案
+if os.path.isfile('favorite.csv'):
+	print('Yeah! Find the file')
+	with open('favorite.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if 'artist, song' in line:
+				continue
+			artist, music = line.strip().split(',')
+			music.append([artist, song])
+	print(music)
+else:
+	print('Oh no...the file can not be found.')
 
 # 請使用者輸入
 while True:
@@ -8,9 +20,11 @@ while True:
 			break
 	song = input('Which his/her/their song do you like the most? ')
 	music.append([artist, song])
+
 # 印出喜愛列表
 for m in music:
 	print('You are the fan of', m[0], 'and loving the song', m[1])
+
 # 寫入檔案
 with open('favorite.csv', 'w', encoding='utf-8') as f:
 	f.write('artist, song\n')
